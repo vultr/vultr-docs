0cf101eb9fe7e738471bd667a6bb67c967d9dd15
David Finster <dfinster@vultr.com>
2021-03-19 17:53:55 -0400

    Update apt-setup.sh

--- faq/vultr-marketplace/apt/apt-setup.sh
+++ faq/vultr-marketplace/apt/apt-setup.sh
@@ -48,6 +48,18 @@ cat << "EOFINSTANCE" > /var/lib/cloud/scripts/per-instance/provision.sh
 #!/bin/bash
 ## Runs once-and-only-once at first boot per instance.
 echo $(date -u) ": System provisioned." >> /var/log/per-instance.log
+
+## Capture Marketplace Password Variables
+DEMO_PASSWORD=$(curl -H "METADATA-TOKEN: vultr" http://169.254.169.254/v1/internal/app-demo_password)
+WEB_PASSWORD=$(curl -H "METADATA-TOKEN: vultr" http://169.254.169.254/v1/internal/app-web_password)
+
+## Create user
+adduser demo --gecos "First Last,RoomNumber,WorkPhone,HomePhone" --disabled-password
+## Set Password
+echo demo:$DEMO_PASSWORD | chpasswd
+
+## Report the Web Password
+echo "The Web password is: $WEB_PASSWORD" > /root/web-password.txt
 EOFINSTANCE
 
 # Make the scripts executable
