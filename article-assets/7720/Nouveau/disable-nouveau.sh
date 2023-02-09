ca267d85e287a25284b2604d78bf7ab4bdbf5f0d
David Finster <david@fortc.com>
2023-02-09 17:24:00 -0500

    reformat code

---
+++ article-assets/7720/Nouveau/disable-nouveau.sh
@@ -0,0 +1,12 @@
+$ BLACKLIST_FILE=$(cat <<ENDFILE
+blacklist nouveau
+options nouveau modeset=0
+ENDFILE
+)
+$ MODPROBE_FILE=$(cat <<ENDFILE
+nvidia
+nvidia_vgpu_vfio
+ENDFILE
+)
+$ echo "${MODPROBE_FILE}" > /etc/modules-load.d/nvidia.conf
+$ echo "${BLACKLIST_FILE}" > /etc/modprobe.d/blacklist-nouveau.conf
